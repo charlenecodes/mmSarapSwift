b4d94736056f4a5620242d6915a341e63efebbb3
Charlene <a43759793@w01bf6no.local>
2024-03-18 09:35:20 +0100

    first commit

---
+++ mmSarapSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mmSarapSwift
+//
+//  Created by Charlene Lava on 3/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mmSarapSwift/mmSarapSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  mmSarapSwiftApp.swift
+//  mmSarapSwift
+//
+//  Created by Charlene Lava on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct mmSarapSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
